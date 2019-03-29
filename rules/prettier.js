e0ec5627e6e1df8fd9894b2e396c8f547fee86d1
G.Kawin <g.kawin@live.com>
2019-03-29 16:01:09 +0700

    ignore prettierrc file

--- rules/prettier.js
+++ rules/prettier.js
@@ -7,6 +7,7 @@ module.exports = {
     "prettier/prettier": [
       "error",
       {
+        usePrettierrc: false,
         singleQuote: true,
         trailingComma: "all",
         semi: false,
